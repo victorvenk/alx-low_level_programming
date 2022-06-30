db515dc1694224c2f88a10b73d2f497ea80980bc
victorvenk <victorolamy@gmail.com>
2022-06-30 16:01:38 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the phrase
+ * the outcoe is a piece of art
+ * Return: 0 if exited properly
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
