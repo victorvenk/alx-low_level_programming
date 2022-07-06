49d5c8799ba38b0cb29efdf1be0a3465972f60cb
Victor Ogunniran <86237353+victorvenk@users.noreply.github.com>
2022-07-06 05:55:11 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
-i
+#include "main.h"
+
+/**
+*Print alphabet ib lower case
+*/
+
+void print_alphabet(void) {
+  char ch;
+  
+  ch = 'a';
+  
+  while (ch <= 'z') {
+    _putchar(ch);
+    ch++;
+  }
+  
+  _putchar("\n");
+}
