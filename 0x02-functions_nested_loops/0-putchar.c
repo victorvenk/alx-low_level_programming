6de2222a859b21772bb100f2024b8885cf7d548d
Victor Ogunniran <86237353+victorvenk@users.noreply.github.com>
2022-07-06 05:43:32 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,19 @@
-i
+#include <main.h>
+
+/**
+*Print _putchar by a new line
+*return success 0
+*/
+
+int main(void) {
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n')
+  
+  return(0);
+}
